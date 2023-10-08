53fce87bc2296f7fe8843867768ffa08a14e6385
julia semenuk <julia.semenuk@gmail.com>
2023-10-08 23:25:45 +0300

    Update review.py

--- review.py
+++ review.py
@@ -121,7 +121,7 @@ with st.form("my_form", clear_on_submit=False):
 
 
 # summary calculation
-summary = '\n- '.join(v for k,v in sorted(st.session_state.items())
+summary = '- '+'\n- '.join(v for k,v in sorted(st.session_state.items())
                        if k.startswith('item') and len(v)>0) + '\n\n🤓'
 
 rating, class_score = process_rate()
